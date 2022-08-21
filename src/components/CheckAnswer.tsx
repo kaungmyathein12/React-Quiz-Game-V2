9ac36f2fbee27a8eee55eb71e0a0cb5c7663e69d
kaungmyathein982 <kaungmyathein.dev@gmail.com>
2022-08-21 19:22:29 +0630

    update

--- src/components/CheckAnswer.tsx
+++ src/components/CheckAnswer.tsx
@@ -1,36 +1,20 @@
 interface Props {
-  btn: string;
   checkAns: () => void;
-  marks: number | null;
-  quizesLength: number;
 }
 const CheckAnswer: React.FC<Props> = (props) => {
   const checkBtn = (
     <>
       <button
         className={"bg-green-500 text-black font-semibold px-4 py-2 rounded-md"}
-        onClick={
-          props.btn === "Check Answer"
-            ? props.checkAns
-            : () => {
-                window.location.reload();
-              }
-        }
+        onClick={props.checkAns}
       >
-        {props.btn}
+        Check Answer
       </button>
     </>
   );
   return (
     <div className="flex flex-row justify-start gap-x-5 items-center">
       {checkBtn}
-      {props.marks !== null ? (
-        <p>
-          You scored {props.marks} out of {props.quizesLength}
-        </p>
-      ) : (
-        <></>
-      )}
     </div>
   );
 };
